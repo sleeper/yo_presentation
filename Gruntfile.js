c2b617e5454b4d9c7858b960d0beb79c0798468f
vagrant <vagrant@precise32.(none)>
2013-07-01 12:26:40 +0000

    WIP

--- Gruntfile.js
+++ Gruntfile.js
@@ -54,9 +54,10 @@ module.exports = function (grunt) {
         },
         connect: {
             options: {
-                port: 9000,
+                port: 8080,
                 // change this to '0.0.0.0' to access the server from outside
-                hostname: 'localhost'
+                // hostname: 'localhost'
+                hostname: '0.0.0.0'
             },
             livereload: {
                 options: {
